116ee8a94f8545f88b7449a0e050af846bba1739
amisha.darji <amisha.d@mail.biztechcs.com>
2025-07-31 17:27:19 +0530

    FIX: Bulk Buy Migrate Issue

--- theme_scita/controllers/bulk_buy_product_discount.py
+++ theme_scita/controllers/bulk_buy_product_discount.py
@@ -9,17 +9,17 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleExtended(WebsiteSale):
 
     @http.route(['/shop/<model("product.template"):product>'], type='http', auth="public", website=True,
-                sitemap=WebsiteSale.sitemap_products, readonly=True)
+                sitemap=WebsiteSale.sitemap_products)
     def product(self, product, category='', search='', **kwargs):
         if not request.website.has_ecommerce_access():
             return request.redirect('/web/login')
 
         values = self._prepare_product_values(product, category, search=search, **kwargs)
 
-        order_sudo = request.cart or request.website._create_cart(force_create=True)
+        order_sudo = request.cart or request.website._create_cart()
         if order_sudo.state != 'draft':
             request.website.sale_reset()
-            order_sudo = request.cart or request.website._create_cart(force_create=True)
+            order_sudo = request.cart or request.website._create_cart()
 
         # Add the order to the template context
         values.update({
